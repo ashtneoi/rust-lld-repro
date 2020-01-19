99b7abeb1d0e4222dfe073b8b30e844316701862
ashtneoi <ashtneoi@gmail.com>
2020-01-19 14:04:22 -0800

    Repro even when changing location counter

--- src/main.rs
+++ src/main.rs
@@ -20,18 +20,23 @@ static STUFF: &[u8] = b"here's a string that we're going to read from";
 #[start]
 fn _start(argc: isize, _argv: *const *const u8) -> isize {
     unsafe {
-        bar(STUFF[argc as usize])
+        foo_dummy(bar_dummy(STUFF[argc as usize])) as isize
     }
 }
 
 extern "C" {
-    fn bar(sth: u8) -> isize;
+    fn foo_dummy(sth: u8) -> u8;
+    fn bar_dummy(sth: u8) -> u8;
 }
 
 global_asm!(
     r#"
         .section foo, "ax"
-        bar:
+        foo_dummy:
+            ret
+
+        .section bar, "ax"
+        bar_dummy:
             ret
     "#;
 );
